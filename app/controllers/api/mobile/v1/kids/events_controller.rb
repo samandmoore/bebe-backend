a3572162a6ec8935cd6fe6aa5240661281f06c1f
Sam Moore <sam@betterment.com>
2022-10-01 18:21:17 -0400

    sort events

--- app/controllers/api/mobile/v1/kids/events_controller.rb
+++ app/controllers/api/mobile/v1/kids/events_controller.rb
@@ -1,7 +1,7 @@
 class Api::Mobile::V1::Kids::EventsController < ApplicationController
   def index
     # TODO: paginate with cursor...
-    events = current_user.kids.find(params[:kid_id]).events
+    events = current_user.kids.find(params[:kid_id]).events.order(started_at: :desc)
     render :index, locals: { cursor: nil, events: events }
   end
 end
